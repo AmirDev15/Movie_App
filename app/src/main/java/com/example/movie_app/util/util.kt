d878fe853ecbd7f30754d0c976a3f4d76b290f53
Amir <amirfadhil.abd@gmail.com>
2024-07-05 18:46:34 +0300

    adding new api endpoint

--- app/src/main/java/com/example/movie_app/util/util.kt
+++ app/src/main/java/com/example/movie_app/util/util.kt
@@ -1,9 +1,12 @@
 package com.example.movie_app.util
 
 object util {
-
     // https://moviesapi.ir/api/v1/movies?/page=1  this is the url where the api is hosted and requesting chunk of pages from large data.
-    const val Base = "https://moviesapi.ir/api/v1/"
-    //const val Base= "https://netflixroulette.net/api/"
+    const val Base = "https://moviesapi.ir/api/v1/"// some times API Rate Limits: causes the the    HomeScreen
+    //of my app to now show any movies data . so incase we can use this app , but it needs some configuration and changes due to having api keys, and some functions need to be changed.
+    // const val new_api = https://api.themoviedb.org/3/discover/movie?&api_key=8eb0491e321882913ee0160dcca92f5a
+    // we can also use this Api url, for fetching movie data.
+    //const val api_key = "8eb0491e321882913ee0160dcca92f5a"
+
 
 }
