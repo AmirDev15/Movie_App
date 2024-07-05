9ee9b71513178061e59f5e3a459ea9f22fa4515f
Amir <amirfadhil.abd@gmail.com>
2024-07-05 18:37:13 +0300

    user and password for login page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,22 +68,22 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     // retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 
     //
-    implementation("androidx.compose.ui:ui:1.0.0")
-    implementation("androidx.compose.material:material:1.0.0")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.0.0")
-    implementation("androidx.activity:activity-compose:1.3.0")
-    implementation("androidx.compose.material:material-icons-extended:1.0.0")
+    implementation("androidx.compose.ui:ui:1.6.8")
+    implementation("androidx.compose.material:material:1.6.8")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.8")
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.compose.material:material-icons-extended:1.6.8")
 
     //coil
-    implementation ("io.coil-kt:coil-compose:2.4.0")
-    implementation ("com.google.accompanist:accompanist-coil:0.10.0")
+    implementation ("io.coil-kt:coil-compose:2.6.0")
+    implementation ("com.google.accompanist:accompanist-coil:0.15.0")
 
     // viewModel
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.3")
 
     //navigation
     implementation("androidx.navigation:navigation-compose:2.7.7")
---
+++ app/src/main/java/com/example/movie_app/Usename$password.kt
@@ -0,0 +1,4 @@
+package com.example.movie_app
+
+//email:amirfadhil.abd@gmail.com
+//pass:Amir1234@@
