e8fa7a9ce6e3553ec0e77f1dea704d47d5d5a418
Amir <amirfadhil.abd@gmail.com>
2024-07-09 19:16:57 +0300

    Update build.gradle.kts
    adding moshi dependencies with retorfit converter to moshi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,7 +88,11 @@ dependencies {
     //navigation
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
+//    moshi library'
 
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.moshi:moshi:1.12.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.12.0")
 
 
 }
